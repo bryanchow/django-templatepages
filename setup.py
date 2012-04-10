6ece35197cf6c40021bfc435ed2be06263eb4d3a
Matt Lynch <njdevil@gmail.com>
2012-04-10 12:00:25 -0400

    Upgraded setup.py, added requirements.txt, README.md for the new PyPi server

--- setup.py
+++ setup.py
@@ -1,15 +1,18 @@
+#!/usr/bin/env python
+
 from setuptools import setup, find_packages
 import sys, os
 
 version = '0.1'
 
+def read_file(the_file):
+    current_dir = os.path.normpath(os.path.dirname(__file__))
+    return open(os.path.join(current_dir, the_file)).read()
+
 setup(name='templatepages',
       version=version,
-      description=("django-templatepages is a Django app for mapping URLs to "
-                   "templates on the filesystem as if they were static web "
-                   "pages"),
-      long_description="""\
-""",
+      description="django-templatepages is a Django app for mapping URLs to templates on the filesystem as if they were static web pages"),
+      long_description=read_file('README.md'),
       classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       keywords='',
       author='Brian Chow and Ian Clelland',
@@ -19,7 +22,5 @@ setup(name='templatepages',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
       zip_safe=False,
-      entry_points="""
-      # -*- Entry points: -*-
-      """,
+      install_requires=read_file('requirements.txt'),
       )
