6be01c8369f15b0bce22f5457bc3fd50a3593e88
Eric Moritz <emoritz@gannett.com>
2012-04-24 15:14:08 -0400

    Fixed the bad merge of setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,7 @@
-<<<<<<< HEAD
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.1'
+version = '0.1.2-gannett'
 
 setup(name='django-templatepages',
       version=version,
@@ -17,7 +16,7 @@ leveraging the Django templating language.
       keywords='',
       author='Bryan Chow',
       author_email='',
-      url='https://github.com/bryanchow/django-templatepages',
+      url='https://github.com/GannettDigital/django-templatepages',
       license='BSD',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
@@ -29,31 +28,4 @@ leveraging the Django templating language.
       entry_points="""
       # -*- Entry points: -*-
       """,
-=======
-#!/usr/bin/env python
-
-from setuptools import setup, find_packages
-import os
-
-version = '0.1'
-
-def read_file(the_file):
-    current_dir = os.path.normpath(os.path.dirname(__file__))
-    return open(os.path.join(current_dir, the_file)).read()
-
-setup(name='templatepages',
-      version=version,
-      description="django-templatepages is a Django app for mapping URLs to templates on the filesystem as if they were static web pages",
-      long_description=read_file('PKG-INFO'),
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      keywords='',
-      author='Brian Chow and Ian Clelland',
-      author_email='',
-      url='https://github.com/bryanchow/django-templatepages',
-      license='',
-      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[],
->>>>>>> 6bca31670af0bdc8718cc4ee0c81e8e1c4432477
       )
